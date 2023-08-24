4de248486d601487cc33f20bf67d88a9aece142d
James Snape <james.snape@gmail.com>
2023-08-24 16:01:24 +0000

    Pages publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [],
+    site: `https://snape.me`,
+    output: 'static',
+    outDir: './docs',
+    build: {
+        assets: 'astro'
+    }
+});
